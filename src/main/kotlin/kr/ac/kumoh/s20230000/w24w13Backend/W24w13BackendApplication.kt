ebda824fb88d1477153d8ba3ce2b5809e5bd909c
BWOH <bwoh@outlook.kr>
2024-11-26 22:39:39 +0900

    🎉 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20230000/w24w13Backend/W24w13BackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20230000.w24w13Backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w13BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w13BackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20230000/w24w13Backend/W24w13BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20230000.w24w13Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w13BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
